a18cac021ea0cf8219944ec911d4ae98ae77a522
Frank10022002 <franklin022002@gmail.com>
2026-04-04 03:00:31 -0500

    yo

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -377,7 +377,7 @@ export const Gallery: React.FC = () => {
                 <div className="h-px bg-slate-100 w-full" />
                 <div className="flex flex-col items-center gap-2">
                   <h4 className="text-[9px] font-black text-slate-300 uppercase tracking-[0.2em]">
-                    Subido por
+                    Subido por:
                   </h4>
                   <div className="flex items-center gap-3 bg-slate-50 px-4 py-2 rounded-2xl border border-slate-100">
                     <img
